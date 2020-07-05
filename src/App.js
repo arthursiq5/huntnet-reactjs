5051618802d89404a1b8af9ce96117ddedbb58cf
Arthur Siqueira <arthur.siq5@gmail.com>
2020-07-05 15:44:01 -0300

    importando component na tela principal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Main from './pages/main';
 
 import './styles.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
