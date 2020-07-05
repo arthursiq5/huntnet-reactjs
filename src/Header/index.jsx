a9a12bdad335e0347cc07caacd2f947f59375e2c
Arthur Siqueira <arthur.siq5@gmail.com>
2020-07-04 21:15:50 -0300

    criado Header para a aplicação

---
+++ src/Header/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Header from './Header';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
