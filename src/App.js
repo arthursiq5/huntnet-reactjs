0cfde72f5eedf4e95a24b9472812981c5ab9cc29
Arthur Siqueira <arthur.siq5@gmail.com>
2020-07-13 20:55:02 -0300

    Usando router no componente principal da tela

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Main from './pages/main';
+import Routes from './routes';
 
 import './styles.css';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes/>
     </div>
   );
 }
